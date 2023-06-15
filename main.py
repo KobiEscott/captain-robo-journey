e5a9c2f51ce95997b44fa362ab5ef5ad9f77a9ed
KobiEscott <136233148+KobiEscott@users.noreply.github.com>
2023-06-15 20:18:04 +1000

    Update pxt.json, main.ts, main.py

--- main.py
+++ main.py
@@ -1,3 +1,87 @@
-def on_forever():
-    pass
-basic.forever(on_forever)
+def Forward():
+    PCA9685.set_led_duty_cycle(PCA9685.LEDNum.LED1, 0, 67)
+    PCA9685.set_led_duty_cycle(PCA9685.LEDNum.LED2, 40, 67)
+    PCA9685.set_led_duty_cycle(PCA9685.LEDNum.LED3, 0, 67)
+    PCA9685.set_led_duty_cycle(PCA9685.LEDNum.LED4, 42, 67)
+def Colours(pause2: number):
+    strip.show_color(neopixel.colors(NeoPixelColors.RED))
+    basic.pause(pause2)
+    strip.show_color(neopixel.colors(NeoPixelColors.ORANGE))
+    basic.pause(pause2)
+    strip.show_color(neopixel.colors(NeoPixelColors.YELLOW))
+    basic.pause(pause2)
+    strip.show_color(neopixel.colors(NeoPixelColors.GREEN))
+    basic.pause(pause2)
+    strip.show_color(neopixel.colors(NeoPixelColors.BLUE))
+    basic.pause(pause2)
+    strip.show_color(neopixel.colors(NeoPixelColors.INDIGO))
+    basic.pause(pause2)
+    strip.show_color(neopixel.colors(NeoPixelColors.VIOLET))
+    basic.pause(pause2)
+    strip.show_color(neopixel.colors(NeoPixelColors.PURPLE))
+def TurnLeft():
+    PCA9685.set_led_duty_cycle(PCA9685.LEDNum.LED1, 0, 67)
+    PCA9685.set_led_duty_cycle(PCA9685.LEDNum.LED2, 20, 67)
+    PCA9685.set_led_duty_cycle(PCA9685.LEDNum.LED3, 0, 67)
+    PCA9685.set_led_duty_cycle(PCA9685.LEDNum.LED4, 55, 67)
+
+def on_button_pressed_a():
+    QuestionTwo()
+input.on_button_pressed(Button.A, on_button_pressed_a)
+
+def Back():
+    PCA9685.set_led_duty_cycle(PCA9685.LEDNum.LED1, 100, 67)
+    PCA9685.set_led_duty_cycle(PCA9685.LEDNum.LED2, 35, 67)
+    PCA9685.set_led_duty_cycle(PCA9685.LEDNum.LED3, 100, 67)
+    PCA9685.set_led_duty_cycle(PCA9685.LEDNum.LED4, 35, 67)
+def QuestionTwo():
+    CountDown()
+    Forward()
+    basic.pause(1500)
+    Stop()
+    music.play_tone(494, music.beat(BeatFraction.DOUBLE))
+    basic.pause(1000)
+    Forward()
+    basic.pause(700)
+    Stop()
+def Rainbow(pause22: number):
+    strip.show_rainbow(1, 360)
+    basic.pause(100)
+def TurnRight():
+    PCA9685.set_led_duty_cycle(PCA9685.LEDNum.LED1, 0, 67)
+    PCA9685.set_led_duty_cycle(PCA9685.LEDNum.LED2, 55, 67)
+    PCA9685.set_led_duty_cycle(PCA9685.LEDNum.LED3, 0, 67)
+    PCA9685.set_led_duty_cycle(PCA9685.LEDNum.LED4, 20, 67)
+def QuestionOne():
+    CountDown()
+    TurnRight()
+    basic.pause(50)
+    Forward()
+    basic.pause(1400)
+    Stop()
+    music.play_tone(262, music.beat(BeatFraction.DOUBLE))
+    basic.pause(1000)
+    TurnLeft()
+    basic.pause(900)
+    Forward()
+    basic.pause(200)
+    Stop()
+def CountDown():
+    global count
+    count = 2
+    while count > 0:
+        basic.show_number(count)
+        music.play_tone(349, music.beat(BeatFraction.HALF))
+        basic.pause(200)
+        count += 0 - 1
+    music.play_melody("E B C5 A B G A F ", 120)
+def Stop():
+    PCA9685.set_led_duty_cycle(PCA9685.LEDNum.LED1, 0, 67)
+    PCA9685.set_led_duty_cycle(PCA9685.LEDNum.LED2, 0, 67)
+    PCA9685.set_led_duty_cycle(PCA9685.LEDNum.LED3, 0, 67)
+    PCA9685.set_led_duty_cycle(PCA9685.LEDNum.LED4, 0, 67)
+count = 0
+strip: neopixel.Strip = None
+PCA9685.init(67, 0)
+strip = neopixel.create(DigitalPin.P5, 18, NeoPixelMode.RGB)
+strip.clear()
--- main.ts
+++ main.ts
@@ -1,10 +1,11 @@
-function Forward () {
+function Forward() {
     PCA9685.setLedDutyCycle(PCA9685.LEDNum.LED1, 0, 67)
     PCA9685.setLedDutyCycle(PCA9685.LEDNum.LED2, 40, 67)
     PCA9685.setLedDutyCycle(PCA9685.LEDNum.LED3, 0, 67)
     PCA9685.setLedDutyCycle(PCA9685.LEDNum.LED4, 42, 67)
 }
-function Colours (pause2: number) {
+
+function Colours(pause2: number) {
     strip.showColor(neopixel.colors(NeoPixelColors.Red))
     basic.pause(pause2)
     strip.showColor(neopixel.colors(NeoPixelColors.Orange))
@@ -21,22 +22,25 @@ function Colours (pause2: number) {
     basic.pause(pause2)
     strip.showColor(neopixel.colors(NeoPixelColors.Purple))
 }
-function TurnLeft () {
+
+function TurnLeft() {
     PCA9685.setLedDutyCycle(PCA9685.LEDNum.LED1, 0, 67)
     PCA9685.setLedDutyCycle(PCA9685.LEDNum.LED2, 20, 67)
     PCA9685.setLedDutyCycle(PCA9685.LEDNum.LED3, 0, 67)
     PCA9685.setLedDutyCycle(PCA9685.LEDNum.LED4, 55, 67)
 }
-input.onButtonPressed(Button.A, function () {
+
+input.onButtonPressed(Button.A, function on_button_pressed_a() {
     QuestionTwo()
 })
-function Back () {
+function Back() {
     PCA9685.setLedDutyCycle(PCA9685.LEDNum.LED1, 100, 67)
     PCA9685.setLedDutyCycle(PCA9685.LEDNum.LED2, 35, 67)
     PCA9685.setLedDutyCycle(PCA9685.LEDNum.LED3, 100, 67)
     PCA9685.setLedDutyCycle(PCA9685.LEDNum.LED4, 35, 67)
 }
-function QuestionTwo () {
+
+function QuestionTwo() {
     CountDown()
     Forward()
     basic.pause(1500)
@@ -47,17 +51,20 @@ function QuestionTwo () {
     basic.pause(700)
     Stop()
 }
-function Rainbow (pause2: number) {
+
+function Rainbow(pause22: number) {
     strip.showRainbow(1, 360)
     basic.pause(100)
 }
-function TurnRight () {
+
+function TurnRight() {
     PCA9685.setLedDutyCycle(PCA9685.LEDNum.LED1, 0, 67)
     PCA9685.setLedDutyCycle(PCA9685.LEDNum.LED2, 55, 67)
     PCA9685.setLedDutyCycle(PCA9685.LEDNum.LED3, 0, 67)
     PCA9685.setLedDutyCycle(PCA9685.LEDNum.LED4, 20, 67)
 }
-function QuestionOne () {
+
+function QuestionOne() {
     CountDown()
     TurnRight()
     basic.pause(50)
@@ -72,7 +79,9 @@ function QuestionOne () {
     basic.pause(200)
     Stop()
 }
-function CountDown () {
+
+function CountDown() {
+    
     count = 2
     while (count > 0) {
         basic.showNumber(count)
@@ -82,14 +91,16 @@ function CountDown () {
     }
     music.playMelody("E B C5 A B G A F ", 120)
 }
-function Stop () {
+
+function Stop() {
     PCA9685.setLedDutyCycle(PCA9685.LEDNum.LED1, 0, 67)
     PCA9685.setLedDutyCycle(PCA9685.LEDNum.LED2, 0, 67)
     PCA9685.setLedDutyCycle(PCA9685.LEDNum.LED3, 0, 67)
     PCA9685.setLedDutyCycle(PCA9685.LEDNum.LED4, 0, 67)
 }
+
 let count = 0
-let strip: neopixel.Strip = null
+let strip : neopixel.Strip = null
 PCA9685.init(67, 0)
 strip = neopixel.create(DigitalPin.P5, 18, NeoPixelMode.RGB)
 strip.clear()
